1adbe1eadb38ae2b1aeb3ac675540f9b07d6efc2
tacheraSasi <tacherasasi@gmail.com>
2026-01-07 21:55:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var printLineBreaks, printWords, printChars, printBytes bool
+
+	flag.BoolVar(&printLineBreaks, "l", false, "print line breaks")
+	flag.BoolVar(&printWords, "w", false, "print words")
+	flag.BoolVar(&printChars, "m", false, "print chars")
+	flag.BoolVar(&printBytes, "c", false, "print bytes")
+	
+	filename := flag.CommandLine.Arg(0)
+	flag.Parse()
 }
